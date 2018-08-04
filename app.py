36b4564ff0cfd9537a3ad5d11c7629b4001fa82d
Helder de Melo Mendes <helder.mendes@usp.br>
2018-08-04 07:46:54 -0300

    Adding python wrapper

---
+++ app.py
@@ -0,0 +1,51 @@
+import web3
+from web3 import Web3, HTTPProvider
+from solc import compile_source
+from web3.contract import ConciseContract
+
+
+with open("negotiationplatform.sol","r") as file:
+	src = file.read()
+
+compiled_sol = compile_source(src,import_remappings=['=./', '-'])
+
+contract_interface = compiled_sol['<stdin>:NegotiationPlatform']
+
+
+# web3.py instance
+w3 = Web3(Web3.EthereumTesterProvider())
+
+# set pre-funded account as sender
+w3.eth.defaultAccount = w3.eth.accounts[0]
+
+# Instantiate and deploy contract
+Greeter = w3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
+
+# Submit the transaction that deploys the contract
+tx_hash = Greeter.constructor().transact()
+
+# Wait for the transaction to be mined, and get the transaction receipt
+tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+
+# Create the contract instance with the newly-deployed address
+greeter = w3.eth.contract(
+    address=tx_receipt.contractAddress,
+    abi=contract_interface['abi'],
+)
+
+# Display the default greeting from the contract
+print('Default contract greeting: {}'.format(
+    greeter.functions.greet().call()
+))
+
+print('Setting the greeting to Nihao...')
+tx_hash = greeter.functions.setGreeting('Nihao').transact()
+
+# Wait for transaction to be mined...
+w3.eth.waitForTransactionReceipt(tx_hash)
+
+# Display the new greeting value
+print('Updated contract greeting: {}'.format(
+    greeter.functions.greet().call()
+))
+
